8c505604deb0ee4e3cb2753e43a2d16fba738a68
jorgealgar <lester.str@hotmail.com>
2024-08-29 11:14:21 +0200

    Update Arreglo.js

--- soluciones/Arreglo.js
+++ soluciones/Arreglo.js
@@ -1,13 +1,15 @@
-const array = [1, "Hola", 2, "Mundo"];
-
-const imprimirArreglo = (array) => {
-    
+const imprimirArreglo = (...array) => {
+  let parametros = "";
   for (let index = 0; index < array.length; index++) {
     const element = array[index];
+    if (index == 0) {
+      parametros = parametros + element;
+    } else {
+      parametros = parametros + "\n" + element;
+    }
   }
+  return parametros;
 };
-
-
 
 console.log(imprimirArreglo(1, "Hola", 2, "Mundo"));
 // 1
