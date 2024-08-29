46259b2438b6a529859abe7149ff1290bbd58c6f
jorgealgar <lester.str@hotmail.com>
2024-08-29 09:40:18 +0200

    Arreglo

---
+++ soluciones/Arreglo.js
@@ -0,0 +1,16 @@
+const array = [1, "Hola", 2, "Mundo"];
+
+const imprimirArreglo = (array) => {
+    
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+  }
+};
+
+
+
+console.log(imprimirArreglo(1, "Hola", 2, "Mundo"));
+// 1
+// Hola
+// 2
+// Mundo
