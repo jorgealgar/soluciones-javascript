8ef79962dbfe50f92980bbb6be47c8ad2b3b1355
jorgealgar <lester.str@hotmail.com>
2024-08-29 10:20:26 +0200

    Multiplos

---
+++ soluciones/Multiplo.js
@@ -0,0 +1,18 @@
+const fizzBuzz = (number) => {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return "fizzbuzz";
+    } else if (number % 3 === 0) {
+        return "fizz";
+    } else if (number % 5 === 0) {
+        return "buzz";
+    } else {
+        return number.toString();
+    }
+   
+
+}
+
+console.log(fizzBuzz(6)); // "fizz"
+console.log(fizzBuzz(20)); // "buzz"
+console.log(fizzBuzz(30)); // "fizzbuzz"
+console.log(fizzBuzz(8)); // 8
