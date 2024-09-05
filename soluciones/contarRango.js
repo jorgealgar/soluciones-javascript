7b77c4593fa511587b0c49f94bc41940022f3e8e
jorgealgar <lester.str@hotmail.com>
2024-09-05 09:23:39 +0200

    Update contarRango.js

--- soluciones/contarRango.js
+++ soluciones/contarRango.js
@@ -9,3 +9,9 @@ const contarRango = (a, b) => {
 console.log(contarRango(1, 9)); // 7
 console.log(contarRango(1332, 8743)); // 7410
 console.log(contarRango(5, 6)); // 0
+
+/*
+
+for (DECLARACION DEL INDICE ; CONDICION PARA SEGUIR ITERANDO ; COMO SE MUEVE EL INDICE EN CADA ITERACIÓN)
+
+*/
