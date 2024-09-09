4bdef6337e50b3d2c3c4ff4496ed7779576f79c2
jorgealgar <lester.str@hotmail.com>
2024-09-09 09:35:08 +0200

    Update SumarArreglo.js

--- soluciones/SumarArreglo.js
+++ soluciones/SumarArreglo.js
@@ -1,9 +1,11 @@
-const sumarArreglo = (...array) => {
-    let contador = 0
-    for (let index = 0; index < array.length; index++) {
-        
-        const element = array[index];
-        
-    }
+const sumarArreglo = (array) => {
+  let suma = 0;
+  for (let index = 0; index < array.length; index++) {
+    suma += array[index];
+  }
+  return suma;
+};
 
-}
+console.log(sumarArreglo([3, 1, 2])); // 6
+console.log(sumarArreglo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 55
+console.log(sumarArreglo([])); // 0
